9f969a1ecf2d1a7ffed4898fd1614f371815d96d
Jake Smiley <jake.alan.smiley@gmail.com>
2020-10-15 21:57:25 -0600

    routes added

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static('public'));
 //parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
@@ -112,6 +114,18 @@ app.post('/api/animals', (req, res) => {
         res.json(animal);
     }
 });
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});  
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
